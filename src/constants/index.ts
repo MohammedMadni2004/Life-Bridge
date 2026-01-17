7308acf58510e443029c1ca18a86cdf51c1cba81
Madni <mohammedmadni09@gmail.com>
2026-01-18 02:13:35 +0530

    u

--- src/constants/index.ts
+++
@@ -1,9 +0,0 @@
-export { accessoriesCategories, accessoriesProducts } from './accessoriesProducts';
-export { babiesCategories, babiesProducts } from './babiesProducts';
-export { bagsFootwearCategories, bagsFootwearProducts } from './bagsFootwearProducts';
-export { corporateGiftingCategories, corporateGiftingProducts } from './corporateGiftingProducts';
-export { homeKitchenCategories, homeKitchenProducts } from './homeKitchenProducts';
-export { kidsCategories, kidsProducts } from './kidsProducts';
-export { mensCategories, mensProducts } from './mensProducts';
-export { womensCategories, womensProducts } from './womensProducts';
-
